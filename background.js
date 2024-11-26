8529494f536a90afcc280050a65a5ecc46f96827
Maxence Bailly <65112954+MaxenceBailly@users.noreply.github.com>
2024-11-27 00:13:40 +0100

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
-    const slowScroll = scrollPosition * -0.15;
-    document.body.style.backgroundPosition = `center ${slowScroll}px`;
-  });
-  
